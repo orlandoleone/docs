7150f57ed591b60e861718c9343f964553dead72
orlandoleone <96437604+orlandoleone@users.noreply.github.com>
2023-08-12 13:11:14 +0100

    Update pose locator.py

--- misc/pose locator.py
+++ misc/pose locator.py
@@ -1,94 +1,120 @@
 import cv2
 import tensorflow as tf
 import tensorflow_hub as hub
+import numpy as np
+import glob
 
-name = 'dancer.jpeg'
 
-image_path = '/Users/orlandoleone/Documents/docs/' + name
+seconds = 10
 
-img1 = cv2.imread(image_path)
+#name = 'dancer.jpeg'
 
-dimensions = img1.shape
+#image_path = '/Users/orlandoleone/Documents/docs/' + name
 
-h = img1.shape[0]
-w = img1.shape[1]
-
-buffer = abs(w-h)
-
-if h > w :
-    img = img1[int(buffer/2):h-int(buffer/2), 0:w]
-    h = w
-else :
-    img = img1[0:h, int(buffer/2):w-int(buffer/2)]
-    w = h
-    
-
-cv2.imwrite('docs/' + name, img)
-
-image = tf.io.read_file(image_path)
-image = tf.compat.v1.image.decode_jpeg(image)
-image = tf.expand_dims(image, axis=0)
-
-image = tf.cast(tf.image.resize_with_pad(image, 256, 256), dtype=tf.int32)
-
+#img = cv2.imread(image_path)
 
 model = hub.load('/Users/orlandoleone/Documents/docs/movenet_singlepose_thunder_4')
 movenet = model.signatures['serving_default']
 
-outputs = movenet(image)
-keypoints = outputs['output_0']
-
-print(keypoints)
-
-print(keypoints[0][0][16][0])
-print(keypoints[0][0][14][0])
+h = 1
+w = 1
 
 color = (0, 255, 0)
 
 thick = 7
 
+arr = [[16, 14], [14, 12], [6, 8], [8, 10], [15, 13], [13, 11], [5, 7], [7, 9], [11, 12], [5, 6], [2, 2], [1, 1], [0, 0], [11, 5], [12, 6]]
 
-def plot(starter, ender):
-    start = (int(w*keypoints[0][0][starter][1]), int(h*keypoints[0][0][starter][0]))
-    end = (int(w*keypoints[0][0][ender][1]), int(h*keypoints[0][0][ender][0]))
-    cv2.line(img, start, end, color, thick)
+def bigplotter(frame, i):
 
+    image_path = 'docs/extra/frame_' + str(i) + '.jpeg'
+    
+    img1 = cv2.imread(image_path)
 
-#right
-plot(16, 14)
-plot(14, 12)
-plot(12, 6)
-plot(6, 8)
-plot(8, 10)
+    dimensions = img1.shape
 
-#left
-plot(15, 13)
-plot(13, 11)
-plot(11, 5)
-plot(5, 7)
-plot(7, 9)
+    h = img1.shape[0]
+    w = img1.shape[1]
+    
+    buffer = abs(w-h)
 
-#hips
-plot(11, 12)
+    if h > w :
+        img = img1[int(buffer/2):h-int(buffer/2), 0:w]
+        h = w
+    else :
+        img = img1[0:h, int(buffer/2):w-int(buffer/2)]
+        w = h
 
-#shoulders
-plot(5, 6)
+    cv2.imwrite(image_path, img)
 
-#head
-plot(1, 1)
-plot(2, 2)
-plot(0, 0)
+    # cv2.imwrite('docs/' + name, img)
 
+    image = tf.io.read_file(image_path)
+    image = tf.compat.v1.image.decode_jpeg(image)
+    image = tf.expand_dims(image, axis=0)
+
+    image = tf.cast(tf.image.resize_with_pad(image, 256, 256), dtype=tf.int32)
+
+    outputs = movenet(image)
+    keypoints = outputs['output_0']
+
+    for j in range(15):
+        starter = arr[j][0];
+        ender = arr[j][1]
+
+        if (keypoints[0][0][starter][2] < 0.25 or keypoints[0][0][ender][2] < 0.25) : continue
+        
+        start = (int(w*keypoints[0][0][starter][1]), int(h*keypoints[0][0][starter][0]))
+        end = (int(w*keypoints[0][0][ender][1]), int(h*keypoints[0][0][ender][0]))
+        cv2.line(img, start, end, color, thick)
+    
+
+    cv2.imwrite('docs/extra/frame_' + str(i) + '.jpeg', img)
+
+# cv2.imshow('ORLANDO IS THE GOAT', img)
 
 #add line equation function
 #calculate angles
 
 
-
-cv2.imshow('ORLANDO IS THE GOAT', img)
-
+video = cv2.VideoCapture('/Users/orlandoleone/Documents/docs/Stephen Curry Teaches Shooting, Ball-Handling, and Scoring | Official Trailer | MasterClass (720p).mp4')
 
 
-""" indices = tf.constant([[[0, 0], [0, 1]], [[1, 1], [1, 3]]])
+for i in range(seconds*30):
+    ret, frame = video.read()
+    
+    video.set(cv2.CAP_PROP_POS_FRAMES, i)
+
+    cv2.imwrite('docs/extra/frame_' + str(i) + '.jpeg', frame)
+    
+    bigplotter(frame, i)
+
+
+
+'''
+frameSize = (w, w)
+
+out = cv2.VideoWriter('OUTPUT.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, frameSize)
+
+for filename in glob.glob('/Users/orlandoleone/Documents/docs/extra/*.jpeg'):
+    img = cv2.imread(filename)
+    out.write(img)
+
+out.release()
+print("done")
+
+'''
+
+'''
+cap = cv2.VideoCapture(0)   
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    print("1")
+    
+
+'''
+
+''' indices = tf.constant([[[0, 0], [0, 1]], [[1, 1], [1, 3]]])
  result = tf.gather_nd(keypoints, indices) with tf.Session() as sess:
-    print(sess.run(result)) """
+    print(sess.run(result)) '''
